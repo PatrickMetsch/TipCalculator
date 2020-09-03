65c66f68c9507dfc0e7773b9c99d787c7847ff08
Patrick Metsch <patrickmetsch@Patricks-MacBook-Pro.local>
2020-09-03 01:09:53 -0400

    TipCalculator

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TipCalculator.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TipCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Fifteen = 0;
+            ViewBag.Twenty = 0;
+            ViewBag.TwentyFive = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(TipCalculatorModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Fifteen = model.CalculateTip(15);
+                ViewBag.Twenty = model.CalculateTip(20);
+                ViewBag.TwentyFive = model.CalculateTip(25);
+            }
+            else
+            {
+                ViewBag.Fifteen = 0;
+                ViewBag.Twenty = 0;
+                ViewBag.TwentyFive = 0;
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/TipCalculatorModel.cs
@@ -0,0 +1,20 @@
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TipCalculator.Models
+{
+    public class TipCalculatorModel
+    {
+        [Required(ErrorMessage = "Please enter a meal cost.")]
+        [Range(0.01, Double.MaxValue, ErrorMessage = "Subtotal must be greater than 0")]
+        public double? MealCost { get; set; }
+
+        public double? CalculateTip(int tip)
+        {
+            double tipPercentage = tip * 0.01;
+            return MealCost * tipPercentage;
+        }
+        
+    }
+}
